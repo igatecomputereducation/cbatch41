90f8397bddc928c90064bcdbd883b99133c574a1
igate solutions <igatecomputereducation@gmail.com>
2022-08-16 22:48:12 +0530

    v1

--- prog138.c
+++ prog138.c
@@ -2,15 +2,12 @@
 int main()
 {
 	int i,ll,ul;
-	
 	printf("Lower limit:");
 	scanf("%i",&ll);
 	printf("Upper limit:");
 	scanf("%i",&ul);
-	
 	for(i=ll;i<=ul;i++)
 		printf("%i\n",i);
-
 	return 0;
 }
 
--- prog141.c
+++ prog141.c
@@ -2,13 +2,10 @@
 int main()
 {
 	int n,i,fact;
-	
 	printf("Enter a natural number:");
 	scanf("%i",&n);
-	
 	for(fact=1,i=1;i<=n;i++)
 			fact=fact*i;
-		
 	printf("Factorial %i",fact);
 	return 0;
 }
