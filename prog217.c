390e95f46fe63af5f05cb96a1776208f27935efa
igate solutions <igatecomputereducation@gmail.com>
2022-09-01 10:30:03 +0530

    v1

---
+++ prog217.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int a[50],b[50],n,m,i,j;
+	
+	printf("Size of 1st array:");
+	scanf("%i",&n);
+	
+	printf("Size of 2nd array:");
+	scanf("%i",&m);
+	
+	printf("Enter elements into 1st array\n");
+	for(i=0;i<n;i++)
+		scanf("%i",&a[i]);
+		
+	printf("Enter elements into 2nd array\n");
+	for(i=0;i<m;i++)
+		scanf("%i",&b[i]);
+		
+	for(i=n,j=0;j<m;i++,j++)
+		a[i]=b[j];
+		
+	printf("The 1st array\n");
+	for(i=0;i<n+m;i++)
+		printf("%5i",a[i]);
+	return 0;
+}
+
---
+++ prog220.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a[]={10,20,30,40,50};
+	int i;
+	
+	printf("The given elements are\n");
+	for(i=0;i<5;i++)
+		printf("%5i",a[i]);
+	
+	return 0;
+}
+
